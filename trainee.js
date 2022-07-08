2845cb8531516e6af27ae4115aa7ec27cd402ead
Nika-Firada <nika.firada@gmail.com>
2022-07-08 17:27:02 +0400

    prototypes

--- trainee.js
+++ trainee.js
@@ -1,12 +1,16 @@
-let num = 5;
-let str = 'hello';
-let bool = false;
-console.log(str + bool, str + num, num + bool, str*bool, str*num, num*bool, str/bool, str/num, num/bool);
+// class Animal{
+//     move = 'moving'
+// }
+// const cat = new Animal();
+// console.log(cat.move)
+class Animal{
+    move() {
+        console.log(`i'm moving`);
+    }
+}
 
-let convert1 = String(num);
-let convert2 = String(bool);
-let convert3 = Number(str);
-let convert4 = Number(bool);
-let convert5 = Boolean(str);
-let convert6 = Boolean(num);
-console.log(convert1, convert2, convert3, convert4, convert5, convert6);
+class Cat extends Animal{
+
+}
+let pussy = new Cat();
+pussy.move();
