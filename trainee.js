1a054b98d2569ee5ad5c4d9e158f08b4e264fa8b
Nika-Firada <nika.firada@gmail.com>
2022-07-08 16:55:00 +0400

    arrays

--- trainee.js
+++ trainee.js
@@ -1,12 +1,114 @@
-let num = 5;
-let str = 'hello';
-let bool = false;
-console.log(str + bool, str + num, num + bool, str*bool, str*num, num*bool, str/bool, str/num, num/bool);
+//  1
+function iterateArr(){
+    const movies = ["movie-1", "movie-2", "movie-3"];
+    for (let index = 0; index < movies.length; ++index) {
+        const element = movies[index];
+        console.log(element);
+    }
+    //forEach
+    console.log('forEach')
+    movies.forEach((movie) => {
+        console.log(movie);
+    });
+    //for of
+    console.log('for of')
+    for(const movie of movies){
+        console.log(movie);
+    }
+}
+iterateArr();
 
-let convert1 = String(num);
-let convert2 = String(bool);
-let convert3 = Number(str);
-let convert4 = Number(bool);
-let convert5 = Boolean(str);
-let convert6 = Boolean(num);
-console.log(convert1, convert2, convert3, convert4, convert5, convert6);
+//    2
+console.log('Test 2')
+function arrayToStringAndBack(){
+    let cars = ['Tesla', 'Toyota', 'Mazda'];
+    let carsT = cars.toString();
+    console.log(carsT);
+    let carsBack = carsT.split(',')
+    console.log(carsBack);
+}
+arrayToStringAndBack();
+
+//  3
+console.log('Test 3')
+function addHello(){
+    const friends = ['Dato', 'Lasha', 'Gio']
+    friends.forEach((friend)=>{
+        console.log(`Hello ${friend}`)
+    })
+}
+addHello();
+
+/// 4
+console.log('Test 4')
+function changeBool(){
+    const numeric = [1, 0, 3, 0];
+    numeric.forEach(bool =>{
+        console.log(Boolean(bool));
+    })
+}
+changeBool();
+
+// 5
+console.log('Test 5')
+function sortArr(){
+    const arr = [1,6,7,8,3,4,5,6];
+    arr.sort((a,b)=>{
+        return b-a;
+    })
+    console.log(arr)
+}
+sortArr();
+
+// 6 
+console.log('Test 6')
+function filterArr(){
+    const arr = [1,6,7,8,3,4,5,6]
+    const filtered = arr.filter(num => num > 3);
+    console.log(filtered)
+}
+filterArr();
+
+// 7 
+console.log('Test 7')
+const arr00 = [1, 2, 3, 4, 5, 6];
+function indexArr00(arr, num){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == num){
+            console.log(`index is ${i}`)
+        }
+    }
+}
+indexArr00(arr00, 1);
+
+// 8
+console.log('Test 8')
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+//9
+console.log('Test 9')
+function primeNumbersLoop(min, max){
+    for (let i = min; i <= max; i++) {
+        let flag = 0;
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (i > 1 && flag == 0) {
+            console.log(i);
+        }
+    }
+}
+primeNumbersLoop(13, 23)
+//10
+console.log('test 10')
+function isOdd(number) {
+    if (number % 2 === 0) {
+        return false;
+    }
+    return true;
+}
+console.log(isOdd(7));
