0cb765ec8f5eb659fe5a84a9ea112139d89ee6bd
Nika-Firada <nika.firada@gmail.com>
2022-07-11 13:21:50 +0400

    up

--- trainee.js
+++ trainee.js
@@ -3,6 +3,7 @@
 // }
 // const cat = new Animal();
 // console.log(cat.move)
+/////////////////////////////////////////////////////////
 class Animal{
     move() {
         console.log(`i'm moving`);
@@ -14,3 +15,10 @@ class Cat extends Animal{
 }
 let pussy = new Cat();
 pussy.move();
+//////////////////// without classes.. like spread method?
+
+const animal = {name:'kot'}
+animal.move = "I'm moving";
+const cat = {...animal}
+console.log(animal)
+console.log(cat)
